036c1f2c22560cb9373c300729250b55a079002f
Robert Page <page.synid@gmail.com>
2016-11-12 01:36:08 +0100

    REFACTORs entities to own class for clarity

---
+++ entities.js
@@ -0,0 +1,53 @@
+function getRandomColor() {
+
+    var colors;
+    var index;
+
+    colors = [
+        "white",    "gray",     "black",    "red",  "orange",
+        "yellow",   "green",    "cyan",     "blue", "purple"
+    ];
+    index = generateRandomNumber(0,colors.length);
+    color = colors[index];
+
+    return color;
+
+}
+
+function createEntity() {
+
+    var entity;
+
+    entity = {};
+
+    entity.x = generateRandomNumber(0, 800);
+    entity.y = generateRandomNumber(0, 600);
+
+    entity.color = getRandomColor();
+
+    entity.speed = generateRandomNumber(0, 10) * 0.2;
+
+    entity.status = 1;
+
+    return entity;
+
+}
+
+function getEntities(amount) {
+
+    var entities;
+
+    entities = [];
+
+    var i = 0;
+    var n = amount;
+
+    for ( i; i < n; i += 1 ) {
+
+        entities[i] = createEntity();
+
+    }
+
+    return entities;
+
+}

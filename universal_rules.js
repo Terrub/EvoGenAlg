9f5ea8327efbc2f74eac2a85ce6059fd6778b903
Robert Page <page.synid@gmail.com>
2016-11-26 01:34:01 +0100

    ADDs some quick start notes

--- universal_rules.js
+++ universal_rules.js
@@ -4,6 +4,15 @@
 //  NOTES:
 /*
 
+    Rules is a bit of a separate beast. It should describe the rules that define
+    the project on a very abstract level.
+
+    I'm looking at defining things like conservation of energy here. Possible
+    interactions that occur or how they occur. Like momentum and friction and...
+    well, just anything physics really.
+
+    If a world object loads in these rules it should be able to resolve or
+    collapse the rules into something renderable in some way or another.
 
 */
 

d60d4547188483283df8dae63c26677b04af1a24
Robert Page <page.synid@gmail.com>
2016-11-26 02:51:24 +0100

    ADDs notes to renderer

--- renderer.js
+++ renderer.js
@@ -4,6 +4,15 @@
 //  NOTES:
 /*
 
+    This is the world object. (Might want to rename?)
+    This is where the ruleset is loaded up into and applied on the entities that
+    live here.
+
+    Like plotting a formula. The ruleset is the formula. the entities are the
+    data points and the colours of each data point.
+    The world object then dictates how many data points are visible in the
+    current world frame/viewport and runs the datapoints through the formula to
+    get the actual display data that needs to be sent to the display object.
 
 */
 

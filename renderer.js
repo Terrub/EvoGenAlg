473b658fc6a7450d7c18475a92d986b20bff6956
Robert Page <page.synid@gmail.com>
2016-11-28 08:41:47 +0100

    ADDs notes to renderer

--- renderer.js
+++ renderer.js
@@ -4,17 +4,38 @@
 //  NOTES:
 /*
 
-    This is the world object. (Might want to rename?)
-    This is where the ruleset is loaded up into and applied on the entities that
-    live here.
+    *:  (Action)Points
+    -:  Comments
+    !:  Warnings or urgencies
+    ?:  Questions or requests
+    >:  Answers or results
 
-    Like plotting a formula. The ruleset is the formula. the entities are the
-    data points and the colours of each data point.
-    The world object then dictates how many data points are visible in the
-    current world frame/viewport and runs the datapoints through the formula to
-    get the actual display data that needs to be sent to the display object.
+    -   I don't like having to create a canvas here. But I don't really want to
+        create it over in the html either.
+        Perhaps I should have a separate file that defines the settings?
+        Then again, perhaps I should consider the index to be the settings file?
 
 */
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//  DESCRIPTION:
+/*
+
+    This is the world object. (Might want to rename?)
+    This is where the ruleset is loaded up into and applied on the entities
+    that live here.
+
+    Like plotting a formula. The ruleset is the formula. the entities are
+    the data points and the colours of each data point.
+    The world object then dictates how many data points are visible in the
+    current world frame/viewport and runs the datapoints through the formula
+    to get the actual display data that needs to be sent to the display
+    object.
+
+*/
+
+
 
 "use strict";
 

62ebe1bfeef9b991d067bcc4d39e3f2dd69221f2
Page <page.synid@gmail.com>
2021-07-08 18:10:14 +0200

    Adds some answer to my own thoughts

--- main.js
+++ main.js
@@ -11,14 +11,24 @@ import { Renderer } from './connectors/renderer.js';
 
 /*
   Thoughts:
-  Make entities battle. see who overwrites who more often, that one wins => Bigger genomes have higher survival rate
-  Make entities age based on how many 'actions'/changes they do => Fast burners vs slow simmers.
-    > the two above counteract one another making it more likely a survival of the fittest thing.
+  [√] Make entities battle. see who overwrites who more often, that one wins => Bigger genomes have higher survival rate
+    > See below.
+  [√] Make entities age based on how many 'actions'/changes they do => Fast burners vs slow simmers.
+    * the two above counteract one another making it more likely a survival of the fittest thing.
       Too slow? You get eaten or starve due to new competitions
       Too fast? You die out of starvation due to burning up too much
+      > Fixed this by making each generation resolve a single segment of a genome. Short genomes (len=16, traits=1)
+        pretty much resolve their entire genome each generation.
+        Large genomes would then slowly work down their chain of 'operations' one generation at a time. More complexity
+        means more interaction, but time then becomes a factor, making it possible for fast and nimble pixies to snuff
+        out large lumbering complex pixies.
+
   Make the order of resolving entities in a generation either random (for now) or use a means to order them based on
     speed or number of operations or something?
-    > This should create a more even playing field for all entities across the board.
+    * This should create a more even playing field for all entities across the board.
+      > Fixed this by ordering pixies each generation based on their genome length. Long genomes go first giving them at
+        least one generation to try and overcome multiple fast small genomes that have more than one oppertunity to
+        overpower them.
 */
 
 
